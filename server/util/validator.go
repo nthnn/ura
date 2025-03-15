2023b976feeddea21fd8d4b1b325ec8c449450f9
Nathanne Isip <nathanneisip@gmail.com>
2025-03-15 08:05:08 +0800

    Validation functions on server-side.

--- server/util/validator.go
+++ server/util/validator.go
@@ -1,6 +1,9 @@
 package util
 
-import "regexp"
+import (
+	"net/mail"
+	"regexp"
+)
 
 func IsValidSHA512(hash string) bool {
 	if len(hash) != 128 {
@@ -9,4 +12,58 @@ func IsValidSHA512(hash string) bool {
 
 	match, _ := regexp.MatchString("^[a-fA-F0-9]{128}$", hash)
 	return match
+}
+
+func ValidateUsername(username string) bool {
+	re := regexp.MustCompile(`^[\p{L}\p{N}_.]+$`)
+	return re.MatchString(username)
+}
+
+func ValidateEmail(email string) bool {
+	_, err := mail.ParseAddress(email)
+	return err == nil
+}
+
+func ValidateNumbers(s string) bool {
+	for _, ch := range s {
+		if ch < '0' || ch > '9' {
+			return false
+		}
+	}
+
+	return true
+}
+
+func ValidateSessionToken(s string) bool {
+	if len(s) != 64 {
+		return false
+	}
+
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if !((c >= '0' && c <= '9') ||
+			(c >= 'a' && c <= 'f') ||
+			(c >= 'A' && c <= 'F')) {
+			return false
+		}
+	}
+
+	return true
+}
+
+func ValidateSecurityCode(s string) bool {
+	if len(s) != 32 {
+		return false
+	}
+
+	for i := 0; i < len(s); i++ {
+		c := s[i]
+		if !((c >= '0' && c <= '9') ||
+			(c >= 'a' && c <= 'f') ||
+			(c >= 'A' && c <= 'F')) {
+			return false
+		}
+	}
+
+	return true
 }
