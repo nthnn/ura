0bb53e0a566fa85eef54ba2f6c1c0c9e34ebfd8a
Nathanne Isip <nathanneisip@gmail.com>
2025-03-22 07:16:04 +0800

    Fail safe mechanism for non-successful API responses.

--- server/util/json.go
+++ server/util/json.go
@@ -1,22 +1,51 @@
 package util
 
 import (
+	"bytes"
 	"encoding/json"
 	"net/http"
-	"strconv"
+
+	"github.com/nthnn/ura/logger"
 )
+
+func failedWriteFallback(w http.ResponseWriter) {
+	_, err := w.Write([]byte("{\"status\": \"error\", \"message\": \"Internal error occurred\"}"))
+	if err != nil {
+		logger.Error("Error writing failed fallback response: %s", err.Error())
+	}
+}
 
 func WriteJSON(w http.ResponseWriter, data interface{}) {
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(data)
+	w.WriteHeader(200)
+
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(data); err != nil {
+		logger.Error("Error encoding JSON: %s", err.Error())
+		failedWriteFallback(w)
+
+		return
+	}
+
+	w.Write(buf.Bytes())
 }
 
-func WriteJSONError(w http.ResponseWriter, message string, status int) {
+func WriteJSONError(w http.ResponseWriter, message string) {
 	data := map[string]string{
-		"status":  strconv.Itoa(status),
+		"status":  "error",
 		"message": message,
 	}
 
 	w.Header().Set("Content-Type", "application/json")
-	json.NewEncoder(w).Encode(data)
+	w.WriteHeader(200)
+
+	var buf bytes.Buffer
+	if err := json.NewEncoder(&buf).Encode(data); err != nil {
+		logger.Error("Error encoding JSON: %s", err.Error())
+		failedWriteFallback(w)
+
+		return
+	}
+
+	w.Write(buf.Bytes())
 }
